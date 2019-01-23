6a827b4a0b38da644b6cd34270bb1cda7587ecac
gabriel2anjos <gabriel.anjos.dt@gmail.com>
2019-01-22 22:46:36 -0200

    max_lenght

--- pedidos/models.py
+++ pedidos/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Type(models.Model):
-	name = models.CharField('Nome', unique=True)
+	name = models.CharField('Nome', unique=True, max_length=100)
 	image = models.ImageField(upload_to='pedidos/images', verbose_name='Imagem', blank=True, null=True)
 
 	def __str__(self):
