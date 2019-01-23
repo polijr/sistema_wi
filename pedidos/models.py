0c3e19ea5432ac53b6ae44c36c0226c60ded3620
Rafael Araripe <rafael.araripe@usp.br>
2019-01-23 18:50:14 -0200

    Classe Pedidos

--- pedidos/models.py
+++ pedidos/models.py
@@ -1,7 +1,11 @@
 from django.db import models
+from datetime import datetime 
+
+import datetime
+from datetime import date
 
 class Type(models.Model):
-	name = models.CharField('Nome', unique=True, max_length=100)
+	name = models.CharField('Nome', unique=True)
 	image = models.ImageField(upload_to='pedidos/images', verbose_name='Imagem', blank=True, null=True)
 
 	def __str__(self):
@@ -10,3 +14,12 @@ class Type(models.Model):
 	class Meta:
 		verbose_name = 'Tipo de Pedido'
 		verbose_name_plural = 'Tipos de Pedido'
+
+
+class Pedidos(models.Model):
+	data = models.DateField(auto_now_add=True, blank=True, null=True)
+	tipo = models.ForeignKey(Type, on_delete=models.CASCADE, null=False)
+	caravaneiro = models.BooleanField(default=False)
+	organizador = models.ForeignKey(Organizador, on_delete=models.CASCADE, null=False)
+
+
