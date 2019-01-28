483920d397f5d50c3c54fa2ef0035083fb1df85d
Pkfilho <pedro.filho@polijunior.com.br>
2019-01-28 12:31:35 -0200

    inicio do forms de pedido

---
+++ pedidos/forms.py
@@ -0,0 +1,8 @@
+from django import form
+from usuarios.models import Empresa, Organizador
+
+class PedidosForm(forms.Form):
+    agua = form.
+    coletor = form.
+    staff = form.
+    outro = form.
