6b0570fdf90812f1cff74e62404d746aa2f012d7
iltonandrew <iltonandrew@gmail.com>
2019-01-29 09:08:11 -0200

    atualização dos pedidos

--- pedidos/urls.py
+++ pedidos/urls.py
@@ -1,19 +1,13 @@
 
 
 from django.conf.urls import url, include
-from django.conf.urls import url
 from .models import *
 from .views import *
-
-#app import
-from .views import *
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.views import LogoutView
-
 app_name = 'pedidos'
 
 urlpatterns = [
-	url(r'^pedir/$', login_required(LogoutView.as_view(template_name='pedir.html')), name = 'Pedir'),
+	#url(r'^pedir/$', login_required(LogoutView.as_view()), name = 'Pedir'),
 	url(r'^carregar_ajax/$', CarregarAjax.as_view(), name='Carregar'),
 	url(r'^deletar_ajax/$', DeletarAjax.as_view(), name='Deletar'),
 ]
--- pedidos/views.py
+++ pedidos/views.py
@@ -33,38 +33,42 @@ class DeletarAjax(View):
 			return HttpResponse("Você não tem acesso a essa página")
 
 
-class CadastroEmpresa(View):
-    def get(self, request, *args, **kwargs):
-        current_user = request.user
-        organizador_responsavel = current_user.objects.organizador_resp
-        return render(request, 'pedir.html', {'organizador': organizador})
+# class CadastroEmpresa(View):
+#     def get(self, request, *args, **kwargs):
+#         current_user = request.user
+#         organizador_nome = current_user.usuario.usuario_empresa.organizador_resp.nome
+#         organizador_sobrenome = current_user.usuario.usuario_empresa.organizador_resp.sobrenome
+#		  organizador_telefone = current_user.usuario.usuario_empresa.organizador_resp.telefone
+#         return render(request, 'pedir.html', {'organizador_nome': organizador_nome}, {'organizador_sobrenome': organizador_sobrenome},
+# 																			 {'organizador_telefone': organizador_telefone})
 
-    # def post(self, request, *args, **kwargs):
-    #     form = PedidosForm(request.POST)
-    #     print(form)
-    #     if form.is_valid():
-    #         user = User.objects.create_user(username= form.data['username'],
-    #                                         email=form.data['email'],
-    #                                         password=form.data['password'])
-    #         user.save()
-    #         usuario = Usuario.objects.create(
-    #             user=user,
-    #             cargo=0)
-    #         usuario.save()
-    #         organizador = Organizador.objects.get(pk=request.POST["organizador_resp"])
-    #         empresa = Empresa.objects.create(
-    #             usuario = usuario,
-    #             nome = request.POST["nome"],
-    #             stand = request.POST["stand"],
-    #             tamanho = request.POST["tamanho"],
-    #             palestra = request.POST["palestra"],
-    #             organizador_resp = organizador,
-    #             cnpj = request.POST["cnpj"],
-    #         )
-    #         empresa.save()
-    #         return HttpResponseRedirect('/usuarios/admin')
 
-    #     organizadores = Organizador.objects.all()
-        return render(request, 'pedir.html', {'form': form, 'organizador': organizador})
+#     # def post(self, request, *args, **kwargs):
+#     #     form = PedidosForm(request.POST)
+#     #     print(form)
+#     #     if form.is_valid():
+#     #         user = User.objects.create_user(username= form.data['username'],
+#     #                                         email=form.data['email'],
+#     #                                         password=form.data['password'])
+#     #         user.save()
+#     #         usuario = Usuario.objects.create(
+#     #             user=user,
+#     #             cargo=0)
+#     #         usuario.save()
+#     #         organizador = Organizador.objects.get(pk=request.POST["organizador_resp"])
+#     #         empresa = Empresa.objects.create(
+#     #             usuario = usuario,
+#     #             nome = request.POST["nome"],
+#     #             stand = request.POST["stand"],
+#     #             tamanho = request.POST["tamanho"],
+#     #             palestra = request.POST["palestra"],
+#     #             organizador_resp = organizador,
+#     #             cnpj = request.POST["cnpj"],
+#     #         )
+#     #         empresa.save()
+#     #         return HttpResponseRedirect('/usuarios/admin')
+
+#     #     organizadores = Organizador.objects.all()
+#         return render(request, 'pedir.html', {'form': form, 'organizador': organizador})
 
 
