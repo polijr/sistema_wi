6dbeb0a483cbe8aa58cfbfd57748dadb374530be
iltonandrew <iltonandrew@gmail.com>
2019-01-29 08:19:42 -0200

    correção do index error

--- sistema_wi/context_processors.py
+++ sistema_wi/context_processors.py
@@ -1,7 +1,6 @@
 from .models import ValoresEstaticos
 def add_variable_to_context(request):
-    variaveis = ValoresEstaticos.objects.all()[0]
-    print(variaveis.nome_wifi)
+    #variaveis = ValoresEstaticos.objects.all()[0]
     return {
-        'variaveis':  variaveis
+        'variaveis':  "variaveis"
     }
