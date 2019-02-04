c0a1da11046b387324a3fbcc1f3c220967e1db7d
Pkfilho <pedro.filho@polijunior.com.br>
2019-02-04 10:55:49 -0200

    caravaneiro no banco de dados

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from .models import Usuario, Organizador, Empresa, Gerente
+from .models import Usuario, Organizador, Empresa, Gerente, Caravaneiro
 from sistema_wi.models import ValoresEstaticos
 # Register your models here.
 admin.site.register(Usuario)
@@ -10,3 +10,4 @@ admin.site.register(Organizador)
 admin.site.register(Gerente)
 admin.site.register(Empresa)
 admin.site.register(ValoresEstaticos)
+admin.site.register(Caravaneiro)
