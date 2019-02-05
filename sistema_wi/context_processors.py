30dc7db57ef09edbeecd11fdcd58216ceb97346a
Eduardo Motta <eduardo.motta@polijunior.com.br>
2019-02-05 19:52:58 -0200

    Data de Inicio do Workshop

--- sistema_wi/context_processors.py
+++ sistema_wi/context_processors.py
@@ -1,9 +1,12 @@
 from .models import ValoresEstaticos
+import datetime
 def add_variable_to_context(request):
     try:
         variaveis = ValoresEstaticos.objects.all()[0]
+        dias_pro_workshop = (variaveis.data_de_inicio - datetime.datetime.now().date()).days
         return {
-        'variaveis':  variaveis
+        'variaveis':  variaveis,
+        'dias_pro_workshop': dias_pro_workshop
         }
     except:
         print("Sem valores estaticos no sistema")
--- sistema_wi/models.py
+++ sistema_wi/models.py
@@ -10,5 +10,11 @@ class ValoresEstaticos(models.Model):
     ano_wi = models.CharField(null = False, default="29", blank = False, max_length=5)
     nome_wifi = models.CharField(null = False, default="polijunior", blank = False, max_length=100)
     senha_wifi = models.CharField(null = False, default="polijunior", blank = False, max_length=100)
+    data_de_inicio = models.DateField(null=False, blank=False, default=None)
+
     def __str__(self):
         return "APENAS ALTERAR, NÃO CRIAR!"
+
+    class Meta:
+    	verbose_name = 'Valores Estaticos'
+    	verbose_name_plural = 'Valores Estaticos'
