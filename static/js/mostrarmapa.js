cdfb2eb74568c84e4e90f45b930adc63487b6e25
Pkfilho <pedro.filho@polijunior.com.br>
2019-02-07 12:30:52 -0200

    mapa wi

---
+++ static/js/mostrarmapa.js
@@ -0,0 +1,9 @@
+function bigImg(x) {
+    x.style.height = "400px";
+    x.style.width = "400px";
+  }
+  
+  function normalImg(x) {
+    x.style.height = "160px";
+    x.style.width = "160px";
+  }
