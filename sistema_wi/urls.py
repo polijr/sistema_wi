ede543b1c2ba952c1d5a0eda6bdd72189f44ec41
Rafael Araripe <rafael.araripe@usp.br>
2019-02-19 16:25:10 -0300

    Sem conflitos, migrations resolvidas

--- sistema_wi/urls.py
+++ sistema_wi/urls.py
@@ -37,14 +37,10 @@ urlpatterns = [
     url(r'^documentos/', include((documentos.urls, "documentos"), namespace='documentos')),
     url(r'^informes/', include((informes.urls, "informes"), namespace='informes')),
     url(r'^chat/', include((chat.urls, "chat"), namespace='chat')),
-<<<<<<< HEAD
-    url(r'^definir_feed/$', DefinirDataFeed.as_view())
-    
-
-=======
-    url(r'^valores-sistema/$', sistema_wi.views.ValoresSistema.as_view())
->>>>>>> baf139ee0c9fb32312fc37f08b34647f21423dca
+    url(r'^definir_feed/$', DefinirDataFeed.as_view()),
+    url(r'^valores-sistema/$', sistema_wi.views.ValoresSistema.as_view()),
 ]
+
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
