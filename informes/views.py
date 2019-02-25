2adfb4b8148f61d2b0361b51998fbfe4ffc3ca80
Rafael Araripe <rafael.araripe@usp.br>
2019-02-24 22:30:05 -0300

    Messages dos informes corrigidas

--- informes/views.py
+++ informes/views.py
@@ -10,7 +10,7 @@ class CriarInfo(View):
 	def get(self, request, *args, **kwargs):
 		if request.user.usuario.cargo == 2:
 			form = InformeForm()
-			return render(request, 'criar_informe.html', {'form': form, 'messages': messages, 'post': False})
+			return render(request, 'criar_informe.html', {'form': form})
 		else:
 			return render(request, 'erro_403.html')
 
@@ -18,7 +18,6 @@ class CriarInfo(View):
 		form = InformeForm(request.POST, request.FILES)
 		enviou = False
 		if form.is_valid():
-			informe = form.save()
-			enviou = True
+			form.save()
 			messages.success(request, "Informe criado com sucesso")
-		return render(request, 'criar_informe.html', {'form' : form, 'messages': messages, 'post': True, 'enviou': enviou})
+		return render(request, 'criar_informe.html', {'form': form})
